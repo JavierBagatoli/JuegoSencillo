ecaede1335ed7dd5661f65cae90fe4653d4d03f1
Javier Bagatoli <javi.bagatoli@gmail.com>
2026-07-30 05:52:21 -0300

    update: add images of monster

--- src/assets/monster/monsters.ts
+++ src/assets/monster/monsters.ts
@@ -6,8 +6,24 @@ import sworm from './sworm.png'
 
 
 import slimeCesped from './slime.png'
+import Arbolgante from './t2/Arbolgante.png'
+import caminante from './t2/caminante.png'
+import mimic_planta from './t2/mimic_planta.png'
+import salvaje from './t2/salvaje.png'
+
+
 import slimeElevador from './slime-elevador-espacial.png'
+import robot from './t3/robot.png'
+import centinela from './t3/centinela.png'
+import dron from './t3/dron.png'
+import Torreta from './t3/Torreta.png'
+
 import slimePecio from './slime-pecio.png'
+import ghostTorret from './t4/ghostTorret.png'
+import mutantRat from './t4/mutantRat.png'
+import robotSpider from './t4/robotSpider.png'
+import specter from './t4/specter.png'
+
 
 
 
@@ -20,13 +36,25 @@ const bestiario = {
         sworm
     ],
     monsterT2:[
-        slimeCesped
+        slimeCesped,
+        Arbolgante,
+        caminante,
+        mimic_planta,
+        salvaje,
     ],
     monsterT3:[
-        slimeElevador
+        slimeElevador,
+        robot,
+        centinela,
+        dron,
+        Torreta,
     ],
     monsterT4:[
-        slimePecio
+        slimePecio,
+        ghostTorret,
+        mutantRat,
+        robotSpider,
+        specter,
     ]
 }
 
