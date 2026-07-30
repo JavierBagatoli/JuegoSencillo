2674c1ed62cbdac8223ae071c06339687ea274ad
Javier Bagatoli <javi.bagatoli@gmail.com>
2026-07-30 20:21:19 -0300

    fix: images dungeon

--- src/assets/monster/monsters.ts
+++ src/assets/monster/monsters.ts
@@ -1,31 +1,28 @@
-import slimeMina from './slime-mina.png'
-import stoneGolem from './stone-golem.png'
-import stoneSpider from './stone-spider.png'
-import mole from './mole.png'
-import sworm from './sworm.png'
+import slimeMina from '../../assets/monster/t1/slime-mina.png'
+import stoneGolem from '../../assets/monster/t1/stone-golem.png'
+import stoneSpider from '../../assets/monster/t1/stone-spider.png'
+import mole from '../../assets/monster/t1/mole.png'
+import sworm from '../../assets/monster/t1/sworm.png'
 
 
-import slimeCesped from './slime.png'
-import Arbolgante from './t2/Arbolgante.png'
-import caminante from './t2/caminante.png'
-import mimic_planta from './t2/mimic_planta.png'
-import salvaje from './t2/salvaje.png'
+import slimeCesped from '../../assets/monster/t2/slime.png'
+import Arbolgante from '../../assets/monster/t2/Arbolgante.png'
+import caminante from '../../assets/monster/t2/caminante.png'
+import mimic_planta from '../../assets/monster/t2/mimic_planta.png'
+import salvaje from '../../assets/monster/t2/salvaje.png'
 
 
-import slimeElevador from './slime-elevador-espacial.png'
-import robot from './t3/robot.png'
-import centinela from './t3/centinela.png'
-import dron from './t3/dron.png'
-import Torreta from './t3/Torreta.png'
+import slimeElevador from '../../assets/monster/t3/slime-elevador-espacial.png'
+import robot from '../../assets/monster/t3/robot.png'
+import centinela from '../../assets/monster/t3/centinela.png'
+import dron from '../../assets/monster/t3/dron.png'
+import Torreta from '../../assets/monster/t3/Torreta.png'
 
-import slimePecio from './slime-pecio.png'
-import ghostTorret from './t4/ghostTorret.png'
-import mutantRat from './t4/mutantRat.png'
-import robotSpider from './t4/robotSpider.png'
-import specter from './t4/specter.png'
-
-
-
+import slimePecio from '../../assets/monster/t4/slime-pecio.png'
+import ghostTorret from '../../assets/monster/t4/ghostTorret.png'
+import mutantRat from '../../assets/monster/t4/mutantRat.png'
+import robotSpider from '../../assets/monster/t4/robotSpider.png'
+import specter from '../../assets/monster/t4/specter.png'
 
 const bestiario = {
     monsterT1: [
